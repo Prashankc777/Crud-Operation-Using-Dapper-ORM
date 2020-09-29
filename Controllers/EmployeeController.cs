64c8146f640f5c50bd90c271b25881fb8da5cbde
Prashan Raj Kc <64183754+prkprime1@users.noreply.github.com>
2020-09-29 07:43:02 +0545

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Dapper;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View(DapperOrm.ReturnList<EmployeeModel>("EmployeeViewAll"));
+        }
+        [HttpGet]
+        public ActionResult AddorEdit(int id = 0)
+        {
+            if (id is 0) return View();
+            var param = new DynamicParameters();
+                param.Add("@employeeId",id);
+                return View(DapperOrm.ReturnList<EmployeeModel>("EmployeeViewById", param)
+                    .FirstOrDefault<EmployeeModel>());
+
+        }
+
+        [HttpPost]
+        public ActionResult AddorEdit(EmployeeModel emp)
+        {
+            var param = new DynamicParameters();
+            param.Add("@employeeId",emp.EmployeeId);
+            param.Add("@name",emp.Name);
+            param.Add("@position",emp.Position);
+            param.Add("@age",emp.Age);
+            param.Add("@salary",emp.Salary);
+           DapperOrm.ExceptionWithoutReturn("EmployeeAddOrEdit", param);
+           return RedirectToAction("Index");
+        }
+        public ActionResult Delete (int id)
+        {
+            var param = new DynamicParameters();
+            param.Add("@employeeId", id);
+            DapperOrm.ExceptionWithoutReturn("employeeDeleteBYid", param);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/DapperOrm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using Dapper;
+
+namespace WebApplication5.Models
+{
+    public static class DapperOrm
+    {
+        private static string _connectionString = @"Data Source=.;Initial Catalog=DapperDb;Integrated Security=True;";
+
+
+        public static void ExceptionWithoutReturn(string procedureName, DynamicParameters param = null)
+        {
+            using (var sqlCon = new SqlConnection(_connectionString))
+            {
+
+               sqlCon.Open();
+                sqlCon.Execute(procedureName, param, commandType: CommandType.StoredProcedure);
+            }
+        }
+
+        public static T ExecuteReturnScalar<T>(string procedureName, DynamicParameters param = null)
+        {
+            using (var sqlCon = new SqlConnection(_connectionString))
+            {
+                sqlCon.Open();
+               return (T)Convert.ChangeType(sqlCon.ExecuteScalar(procedureName, param, commandType: CommandType.StoredProcedure), typeof(T));
+            }
+        }
+
+        public static IEnumerable<T> ReturnList<T>(string procedureName, DynamicParameters param = null)
+        {
+            using (var sqlCon = new SqlConnection(_connectionString))
+            {
+
+                sqlCon.Open();
+                return sqlCon.Query<T>(procedureName, param, commandType: CommandType.StoredProcedure);
+            }
+        }
+    }
+}
